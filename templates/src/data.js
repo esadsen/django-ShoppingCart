aa1590b1a7531c8a87764c284c9cf78f2961393f
Blankbee <shen.esad@outlook.com>
2023-03-21 20:40:57 +0300

    first commit

---
+++ templates/src/data.js
@@ -0,0 +1,28 @@
+let shopItems=[{
+    id:"a",
+    name:"Office Shirt",
+    price:36,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-2.jpg",
+},
+{
+    id:"b",
+    name:"Green Suit",
+    price:200,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/suit1.jpg",
+},
+{
+    id:"c",
+    name:"White T-Shirt",
+    price:20,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/tshirt1.jpg",
+},
+{
+    id:"d",
+    name:"Light Blue Jeans",
+    price:34,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/jeans1.jpg",
+    }]
