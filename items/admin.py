f3aab025c7b4947a245a81526c6b53e20fc74207
Blankbee <shen.esad@outlook.com>
2023-04-09 15:21:49 +0300

    added some items and gitignore

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Items
 
 # Register your models here.
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+    list_display=["name","price","desc"]
+    search_fields=["name"]
+
+
+class Meta:
+    model=Items
